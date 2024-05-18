49f131711b4a715dd47b5f5ed55227c383123004
Edward O'Connor <edwardoc@buffalo.edu>
2024-05-18 18:26:37 -0400

    Add middleware to validate request

--- auth-service/src/routes/signin.ts
+++ auth-service/src/routes/signin.ts
@@ -1,10 +1,16 @@
-import express, {Request, Response} from 'express'
+import express, {NextFunction, Request, Response} from 'express'
+import { checkForEmptyField } from '../utils';
+import { RequestValidationError } from '../../errors/request-validation-error';
+import { validateReq } from '../../middleware/validate-req';
 
 const router = express.Router();
 
 /* User Sign In */
-router.get("/api/users/signin", async (res: Response, req: Request) => {
-    return res.send("Hello User");
+router.post("/api/users/signin", validateReq, async (req: Request, res: Response, next: NextFunction) => {
+    const {email, password} = req.body;
+
+   return res.status(200).json(email);
+
 });
 
 
