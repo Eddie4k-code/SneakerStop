663d6694a0922715301760a3d59f0392ab75bbed
Edward O'Connor <edwardoc@buffalo.edu>
2024-05-24 15:30:22 -0400

    retry

--- auth-service/src/routes/__test__/signin.test.ts
+++ auth-service/src/routes/__test__/signin.test.ts
@@ -2,8 +2,19 @@ import request from 'supertest';
 import { app } from '../..';
 
 /* check successful sign in */
+
+
 it('returns a 200 on successful sign in', async () => {
-    return await request(app)
+
+    await request(app)
+      .post('/api/users/signup')
+      .send({
+        email: 'test@mail.com',
+        password: 'password'
+      })
+      .expect(201);
+
+    await request(app)
       .post('/api/users/signin')
       .send({
         email: 'test@mail.com',
