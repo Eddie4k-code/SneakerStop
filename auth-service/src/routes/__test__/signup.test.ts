08977b26d40f5bb50b5d29f95dadfa0baf5d2507
Edward O'Connor <edwardoc@buffalo.edu>
2024-05-24 15:17:58 -0400

    add more tests

--- auth-service/src/routes/__test__/signup.test.ts
+++ auth-service/src/routes/__test__/signup.test.ts
@@ -12,9 +12,9 @@ it('returns a 201 on successful signup', async () => {
       .expect(201);
   });
 
-/* Check empty fields */
-it('returns a 400 ', async () => {
 
+it('returns a 400 ', async () => {
+  /* All empty fields */
   return request(app)
     .post('/api/users/signup')
     .send({
@@ -22,4 +22,36 @@ it('returns a 400 ', async () => {
       password: ""
     })
     .expect(400);
+
+
+  /* 1 empty field */
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@123.com",
+      password: ""
+    })
+    .expect(400);
 });
+
+it('does not allow creating account with email that is already associated with an account, returns 400', async () => {
+  return await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@here.com",
+      password: "password"
+    })
+    .expect(201);
+
+    /* attempt to create account with same email as above*/
+    return await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: "test@here.com",
+      password: "password"
+    })
+    .expect(400);
+
+});
+
+
