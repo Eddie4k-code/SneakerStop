0763718da44be2e3fa7cbb45b27fbf3766132a31
Edward O'Connor <edwardoc@buffalo.edu>
2024-05-24 11:51:08 -0400

    Sign In handler

--- Makefile
+++ Makefile
@@ -3,15 +3,15 @@
 #Installs the ingress-nginx controller and applys all kubernetes infrastructure
 apply:
 	@kubectl create secret generic jwt-secret --from-literal=JWT_SECRET=secret123
-	@cd $(CURDIR)/terraform && curl -sS https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/cloud/deploy.yaml -o ingress-nginx.yaml && \
-	@cd $(CURDIR)/terraform && kubectl apply -f ingress-nginx.yaml && \
-	@echo "Allow time for ingress-nginx to setup..." \
-	@timeout 10 \
-	@cd $(CURDIR)/terraform && terraform init && terraform plan && terraform apply --auto-approve 
+	cd $(CURDIR)/terraform && curl -sS https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/cloud/deploy.yaml -o ingress-nginx.yaml && \
+	cd $(CURDIR)/terraform && kubectl apply -f ingress-nginx.yaml && \
+	echo "Allow time for ingress-nginx to setup..." 
+	timeout 20 
+	cd $(CURDIR)/terraform && terraform init && terraform plan && terraform apply --auto-approve 
 	
 #destorys all kubernetes infrastructure
 destroy:
-	@kubectl delete secret jwt-secret
-	@kubectl delete namespace ingress-nginx \
+	kubectl delete secret jwt-secret
+	kubectl delete namespace ingress-nginx 
 	@cd $(CURDIR)/terraform && terraform init && terraform destroy --auto-approve 
 
--- auth-service/src/routes/signin.ts
+++ auth-service/src/routes/signin.ts
@@ -1,6 +1,9 @@
 import express, {NextFunction, Request, Response} from 'express'
-import { checkForEmptyField } from '../utils';
+import { checkForEmptyField, Password } from '../utils';
 import { RequestValidationError } from '../../errors/request-validation-error';
+import { UserModel } from '../models/user';
+import { GenericRequestError } from '../../errors/generic-request-error';
+import jwt from 'jsonwebtoken';
 
 
 const router = express.Router();
@@ -13,6 +16,37 @@ router.post("/api/users/signin", async (req: Request, res: Response, next: NextF
      if (!checkForEmptyField<string>(email) || !checkForEmptyField<string>(password)) {
         next(new RequestValidationError("Email and Password must be fulfilled."));
         return
+    }
+
+    const existingUser = await UserModel.findOne({email});
+
+    /* Check if user actually exists */
+    if (!existingUser) {
+        next(new GenericRequestError("Incorrect Email or Password"));
+        return
+    }
+
+
+    /* Check hashed pw */
+    const validPassword = Password.validate(existingUser.password as string, password);
+
+    if (!validPassword) {
+        next(new GenericRequestError("Incorrect Email or Password"));
+        return;
+    }
+
+    /* stop app if missing jwt secret as env var */
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT SECRET IS MISSING");
+    }
+
+    const userJWT = jwt.sign({
+        id: existingUser._id,
+        email: existingUser.email
+    }, process.env.JWT_SECRET!);
+
+    req.session = {
+        jwt: userJWT
     }
 
    return res.status(200).json(email);
