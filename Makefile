afabe84f5bfeff1d5ca8f607e5662e104c13e51b
Edward O'Connor <edwardoc@buffalo.edu>
2024-05-25 13:36:52 -0400

    try matrix ci

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
 .PHONY: apply destroy
 
-#Installs the ingress-nginx controller and applys all kubernetes infrastructure
+# Installs the ingress-nginx controller and applies all Kubernetes infrastructure
 apply:
 	@kubectl create secret generic jwt-secret --from-literal=JWT_SECRET=secret123
 	cd $(CURDIR)/terraform && curl -sS https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.0/deploy/static/provider/cloud/deploy.yaml -o ingress-nginx.yaml && \
 	cd $(CURDIR)/terraform && kubectl apply -f ingress-nginx.yaml && \
-	echo "Allow time for ingress-nginx to setup..." 
-	timeout 60 
-	cd $(CURDIR)/terraform && terraform init && terraform plan && terraform apply --auto-approve 
+	echo "Allow time for ingress-nginx to setup..." && \
+	sleep 60 && \
+	cd $(CURDIR)/terraform && terraform init && terraform plan && terraform apply --auto-approve
 	
-#destorys all kubernetes infrastructure
+# Destroys all Kubernetes infrastructure
 destroy:
 	kubectl delete secret jwt-secret
 	@cd $(CURDIR)/terraform && terraform init && terraform destroy --auto-approve 
-	@kubectl delete namespace ingress-nginx 
-
+	@kubectl delete namespace ingress-nginx
---
+++ sneaker-service/src/index.ts
@@ -0,0 +1,54 @@
+import express, {NextFunction, Request, Response} from 'express';
+import { errorHandler } from '@sneakerstop/shared';
+import { NotFoundError } from '@sneakerstop/shared';
+import cookieSession from 'cookie-session';
+import mongoose from 'mongoose';
+
+
+
+export const app = express();
+
+app.set('trust proxy', true); //ingress-nginx proxy
+
+app.use(express.json());
+app.use(
+    cookieSession({
+        signed: false, //JWT will be encrypted.
+        secure: false,
+    })
+)   
+
+
+app.all("*", async (req: Request, res: Response, next: NextFunction) => {
+    next(new NotFoundError());
+});
+
+app.use(errorHandler); 
+
+
+
+
+const start = async () => {
+
+
+    if (!process.env.JWT_SECRET) {
+        throw new Error("JWT_SECRET IS MISSING");
+    }
+
+    try {
+        if (process.env.ENVIRONMENT! != "dev") {
+            await mongoose.connect("mongodb://auth-mongo-srv:27017/sneaker").then(() => console.log("Auth Service Connected to MongoDB"));
+
+            app.listen('3001', () => {
+                console.log("Sneaker Service Running on Port 3000 Successfully");
+            });
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+    
+}
+
+start();
