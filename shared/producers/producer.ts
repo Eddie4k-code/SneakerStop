f5cf624a0bef3038600aa51a87e3008be26c8625
Edward O'Connor <edwardoc@buffalo.edu>
2024-06-01 15:46:38 -0400

    Update Shared Module with Generic kafka producer

---
+++ shared/producers/producer.ts
@@ -0,0 +1,41 @@
+import {Kafka, logLevel, Producer} from 'kafkajs';
+
+abstract class GenericProducer {
+
+    topic: string;
+    protected producer: Producer;
+
+    // config
+    protected kafka = new Kafka({
+        logLevel: logLevel.DEBUG,
+        brokers:["kafka:29092"],
+        clientId: 'sneaker-marketplace',
+    });
+
+    
+    constructor(topic: string) {
+        this.topic = topic;
+
+        this.producer = this.kafka.producer({maxInFlightRequests: 1, idempotent: true, retry: {initialRetryTime: 100, retries:5}}); //EOS Semantics 
+    }
+
+
+    async send<T extends Buffer>(event: T): Promise<void> {
+
+        await this.producer.connect();
+
+        await this.producer.send({
+            topic: this.topic,
+            messages: [{value: event}],
+            acks: -1
+        });
+
+
+        console.log(`Sucessfully Produced to ${this.topic}`);
+        
+
+        await this.producer.disconnect();
+
+    }
+
+}
