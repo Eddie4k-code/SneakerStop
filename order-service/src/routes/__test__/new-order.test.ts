ab113be6634a134e16964e882460bae9bf6bdb19
Edward O'Connor <edwardoc@buffalo.edu>
2024-06-22 13:08:38 -0400

    add another test

--- order-service/src/routes/__test__/new-order.test.ts
+++ order-service/src/routes/__test__/new-order.test.ts
@@ -1,6 +1,9 @@
 import request from 'supertest';
 import { app } from '../..';
 import mongoose from 'mongoose';
+import { SneakerModel } from '../../models/sneaker';
+import { OrderModel } from '../../models/order';
+import { OrderStatus } from '@sneakerstop/shared';
 
 
 it('returns not found if the sneaker does not exist', async () => {
@@ -18,6 +21,35 @@ it('returns not found if the sneaker does not exist', async () => {
 
 
 it('returns an error if a sneaker is in the process of already being ordered', async () => {
+
+
+    const sneaker = SneakerModel.createSneaker({
+        title: 'test_sneaker12',
+        price: 30
+    });
+
+    await sneaker.save();
+
+
+    const order = OrderModel.createOrder({
+        sneaker,
+        userId: 'lasiohwefhioew',
+        status: OrderStatus.Created
+    });
+
+    await order.save();
+
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({
+            sneakerId: sneaker._id
+        })
+        .expect(400);
+
+
+
 
 });
 
