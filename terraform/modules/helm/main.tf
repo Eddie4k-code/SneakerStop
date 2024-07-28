b10e91554e3287b3ea525c6fddfa1bf9bc0f2df8
Edward O'Connor <edwardoc@buffalo.edu>
2024-07-28 16:08:50 -0400

    Add Grafana

--- terraform/modules/helm/main.tf
+++ terraform/modules/helm/main.tf
@@ -1,3 +1,6 @@
+/* This module contains all necessary helm charts */
+
+
 /* Setup ingress */
 resource "helm_release" "ingress-nginx" {
   name       = "ingress-nginx"
@@ -5,4 +8,12 @@ resource "helm_release" "ingress-nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   create_namespace = true
+}
+
+/* Setup Grafana for metric visualization */
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  namespace  = "default"
 }
