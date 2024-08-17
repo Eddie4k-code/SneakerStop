2df3387b89e81629225ba4fcf7c2dd26a7e4e5b8
Edward O'Connor <edwardoc@buffalo.edu>
2024-08-17 16:43:42 -0400

    Add another test to payment service

--- payment-service/src/testing/__test__/new-payment.test.ts
+++ payment-service/src/testing/__test__/new-payment.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import { app } from '../..';
 import mongoose from 'mongoose';
+import { OrderModel } from '../../models/order';
+import { OrderStatus } from '@sneakerstop/shared';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
 
@@ -17,6 +19,23 @@ it('returns a 404 when purchasing an order that does not exist', async () => {
 });
 
 
-it('returns 401 when a order dosent belong to a useer', async () => {
+it('returns 401 when a order dosent belong to a user', async () => {
+    const order = OrderModel.createOrder({ 
+        externalId: new mongoose.Types.ObjectId().toHexString(),
+        userId: new mongoose.Types.ObjectId().toHexString(),
+        status: OrderStatus.Created,
+        price: 150
+    });
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin())
+        .send({
+            token: 'ihofewiohewfhoifew',
+            externalId: order._id
+        })
+        .expect(401);
+
+
 
 });
