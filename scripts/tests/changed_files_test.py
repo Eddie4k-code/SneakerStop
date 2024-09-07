ca1a2dd98628bcfc7df0de5987cf1ce96138eaf3
Edward O'Connor <edwardoc@buffalo.edu>
2024-09-07 16:42:02 -0400

    Test

--- scripts/tests/changed_files_test.py
+++ scripts/tests/changed_files_test.py
@@ -1,27 +1,27 @@
 import unittest
-from scripts.changed_files import get_changed_files
+from scripts.changed_files import changed_files
 
 
 class TestGetChangedFiles(unittest.TestCase):
 
     def test_no_files(self):
-        self.assertEqual(get_changed_files([]), [])
+        self.assertEqual(changed_files.get_changed_files([]), [])
 
     def test_no_matching_services(self):
         files = ["other-service/file.txt", "another-service/anotherfile.txt"]
-        self.assertEqual(get_changed_files(files), [])
+        self.assertEqual(changed_files.get_changed_files(files), [])
 
     def test_some_matching_services(self):
         files = ["auth-service/file1.txt", "order-service/file2.txt", "unrelated/file3.txt"]
-        self.assertEqual(get_changed_files(files), ["auth-service", "order-service"])
+        self.assertEqual(changed_files.get_changed_files(files), ["auth-service", "order-service"])
 
     def test_all_matching_services(self):
         files = ["auth-service/file1.txt", "sneaker-service/file2.txt", "order-service/file3.txt"]
-        self.assertEqual(get_changed_files(files), ["auth-service", "sneaker-service", "order-service"])
+        self.assertEqual(changed_files.get_changed_files(files), ["auth-service", "sneaker-service", "order-service"])
 
     def test_duplicate_services(self):
         files = ["auth-service/file1.txt", "auth-service/file2.txt"]
-        self.assertEqual(get_changed_files(files), ["auth-service"])
+        self.assertEqual(changed_files.get_changed_files(files), ["auth-service"])
 
 if __name__ == "__main__":
     unittest.main()
